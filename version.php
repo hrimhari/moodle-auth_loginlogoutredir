449a408e0f68fecad71e1db11313273d5e78c5f8
Felipe Carasso <felipe@carassonet.org>
2014-10-30 22:00:39 -0400

    - Added missing settings to version.php.

--- version.php
+++ version.php
@@ -29,3 +29,5 @@ defined('MOODLE_INTERNAL') || die;
 $plugin->version   = 2014103000;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2014051200;        // Requires this Moodle version
 $plugin->component = 'auth_loginlogoutredir';      // Full name of the plugin (used for diagnostics)
+$plugin->maturity  = MATURITY_RC;
+$plugin->release   = '1.0 (2014103000)';
